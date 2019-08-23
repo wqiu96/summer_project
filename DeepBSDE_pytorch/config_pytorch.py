d7b371ab485a14ec830a501f20eb1d4701846905
wqiu96 <46581329+wqiu96@users.noreply.github.com>
2019-08-23 15:53:33 -0400

    Update config_pytorch.py

--- DeepBSDE_pytorch/config_pytorch.py
+++ DeepBSDE_pytorch/config_pytorch.py
@@ -42,7 +42,7 @@ class HJBConfig(Config):
     num_iterations = 2000
     lr_values = list(np.array([1e-2, 1e-2]))
     #num_hiddens = [dim, dim+10, dim+10, dim]
-    num_hiddens = [dim, dim+5, dim+5, dim]
+    num_hiddens = [dim, dim+1, dim+1, dim]
     y_init_range = [0, 1]
 
 
